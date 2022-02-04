c93a88e8dcd07169223cc216bd2f43c041f5dcbb
“Your <maria.tasco@udea.edu.co>
2022-02-04 14:40:44 -0500

    se sube el ejercicio 3 complero y bien

--- CSS-FLEXBOX/Trabajos/Trabajo3/Ejercicio3.js
+++ CSS-FLEXBOX/Trabajos/Trabajo3/Ejercicio3.js
@@ -1,10 +1,18 @@
 (function () {
     let movil = document.getElementById('movil');
+
     let contenedorMenu = document.getElementById('contenedorMenu');
+
     // console.log(movil,menu);
 
-    movil.addEventListener ('click', function(){
-        contenedorMenu.style.display = contenedorMenu.style.display == 'flex' ? 'none' : 'flex';
+    movil.addEventListener('click', function(){
+
+	if (contenedorMenu.style.display === 'none') {
+		contenedorMenu.style.display = 'flex';
+	} else {
+		contenedorMenu.style.display = 'none';
+	}
+
     });
 
 }) () ;
